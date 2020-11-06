17dde4e319c6dc010e67bfb42cfd14e346dfb9b8
Nikita Butov <nibuton@gmail.com>
2020-11-06 11:49:01 +0500

    config cleaning

--- src/main/java/com/nibuton/foodclient/configuration/Config.java
+++ src/main/java/com/nibuton/foodclient/configuration/Config.java
@@ -19,11 +19,6 @@ import com.nibuton.foodclient.service.RestService;
 @ComponentScan("com.nibuton.foodclient")
 public class Config {
 	
-	/*@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}*/
-	
 	@Bean
 	public RestTemplateBuilder restTemplateBuilder() {
 		return new RestTemplateBuilder();
@@ -31,36 +26,6 @@ public class Config {
 	
 	@Bean
 	CommandLineRunner testGetAll(RecipeRestService recipeRestService, IngredientRestService ingredientRestService) {
-		/*Recipe recipe = recipeRestService.getById(1);
-		recipe.setDescription("test");
-		recipe.setName("test");
-		System.out.println(recipe);
-		recipeRestService.save(recipe);
-		rec.setId(1);
-		rec.setName("pipka");
-		recipeRestService.save(rec);
-		Ingredient ingredient = ingredientRestService.getById(1);
-		System.out.println(ingredient);
-		ingredient.setRemaining(8.1);
-		ingredientRestService.save(ingredient);
-		Ingredient ing2 = new Ingredient();
-		ing2.setName("salt");
-		ing2.setRemaining(1000000.1);
-		ingredientRestService.save(ing2); */
-		
-		Recipe recipe = recipeRestService.getById(1);
-		System.out.println(recipe);
-		recipe.setDescription("aaa");
-		recipe.setName("cccc");
-		recipeRestService.save(recipe);
-		
-		Ingredient ingredient = ingredientRestService.getById(1);
-		System.out.println(ingredient);
-		ingredient.setRemaining(0.222222);
-		String response = ingredientRestService.save(ingredient).getBody();
-		System.out.println(response);
-		
-		recipeRestService.addNewRecipe(recipe.getId(), ingredient.getId(), 56789.1111);
 	    return args -> { 
 	    };
 	}
